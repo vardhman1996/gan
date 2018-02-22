abd3aa224cb52f73b481baf52d9672369696e666
Vardhman Mehta <mehtav@cs.washington.edu>
2018-02-21 21:34:19 -0800

    converted to cuda

--- models.py
+++ models.py
@@ -4,29 +4,32 @@ import torch.nn.functional as F
 import torch.optim as optim
 from torchvision import datasets, transforms
 from torch.autograd import Variable
+import matplotlib
+matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 import numpy as np
 
-BATCH_SIZE = 32
+BATCH_SIZE = 128
+CUDA = True
 k = 2
 d_learning_rate = 1e-4
 g_learning_rate = 1e-4
 
-
+kwargs = {'num_workers': 1, 'pin_memory': True} if CUDA else {}
 train_loader = torch.utils.data.DataLoader(
     datasets.MNIST('./data', train=True, download=True,
                    transform=transforms.Compose([
                        transforms.ToTensor(),
                        transforms.Normalize((0.1307,), (0.3081,))
                    ])),
-    batch_size=BATCH_SIZE, shuffle=True)
+    batch_size=BATCH_SIZE, shuffle=True, **kwargs)
 
 test_loader = torch.utils.data.DataLoader(
     datasets.MNIST('./data', train=False, transform=transforms.Compose([
                        transforms.ToTensor(),
                        transforms.Normalize((0.1307,), (0.3081,))
                    ])),
-    batch_size=BATCH_SIZE, shuffle=True)
+    batch_size=BATCH_SIZE, shuffle=True, **kwargs)
 
 
 def get_generator_noise_input():
@@ -63,33 +66,31 @@ class Discriminator(nn.Module):
 g_sampler = get_generator_noise_input()
 G = Generator()
 D = Discriminator()
+
+if CUDA:
+    G.cuda()
+    D.cuda()
+
 BCELoss = nn.BCELoss()
 d_optimizer = optim.SGD(D.parameters(), lr=d_learning_rate, momentum=0.9)
 g_optimizer = optim.SGD(G.parameters(), lr=g_learning_rate, momentum=0.9)
 
-# def generative_loss(f_input):
-#     # (1/m) sum_1_m log(1-D(G(z)))
-#     batch_loss = torch.log(torch.add(torch.neg(D(G(f_input))), 1.0))
-#     print "G-Loss:", torch.div(torch.sum(batch_loss), f_input.size(0))
-#     return torch.div(torch.sum(batch_loss), f_input.size(0))
-#
-# def discriminative_loss(r_input, f_input):
-#     # (1/m) sum_1_m log(D(x) + log(1-D(G(z)))
-#     d_loss = torch.log(D(r_input))
-#     g_loss = torch.log(torch.add(torch.neg(D(G(f_input))), 1.0))
-#     batch_loss = torch.add(d_loss, g_loss)
-#     print "D-Loss:", torch.div(torch.sum(batch_loss), r_input.size(0))
-#     return torch.div(torch.sum(batch_loss), r_input.size(0))
-
 def generative_loss(f_data):
     dg_ = D(G(f_data))
-    g_loss = BCELoss(dg_, Variable(torch.zeros(f_data.size()[0], 1)))
+    y_ = torch.zeros(f_data.size()[0], 1)
+    if CUDA:
+        y_ = y_.cuda()
+    g_loss = BCELoss(dg_, Variable(y_))
     return g_loss
 
 
 def discriminative_loss(r_data, f_data):
     d, dg_ = D(r_data), D(G(f_data))
-    d_loss = BCELoss(d, Variable(torch.ones(r_data.size()[0], 1))) + BCELoss(dg_, Variable(torch.zeros(f_data.size()[0], 1)))
+    y, y_ = torch.ones(r_data.size()[0], 1), torch.zeros(f_data.size()[0], 1)
+    if CUDA:
+        y = y.cuda()
+        y_ = y_.cuda()
+    d_loss = BCELoss(d, Variable(y)) + BCELoss(dg_, Variable(y_))
     return d_loss
 
 
@@ -97,17 +98,24 @@ def train_discriminative(data):
     D.train()
     G.eval()
     r_data = Variable(data)
-    f_data = Variable(torch.rand(data.size(0), 1, data.size(2), data.size(3)))
+    z = torch.rand(data.size(0), 1, data.size(2), data.size(3))
+    if CUDA:
+        z = z.cuda()
+    f_data = Variable(z)
     d_optimizer.zero_grad()
     d_loss = discriminative_loss(r_data, f_data)
     d_loss.backward()
     d_optimizer.step()
     return d_loss.data[0]
 
+
 def train_generative(data):
     G.train()
     D.eval()
-    f_data = Variable(torch.rand(data.size(0), 1, data.size(2), data.size(3)))
+    z = torch.rand(data.size(0), 1, data.size(2), data.size(3))
+    if CUDA:
+        z = z.cuda()
+    f_data = Variable(z)
     g_optimizer.zero_grad()
     g_loss = generative_loss(f_data)
     g_loss.backward()
@@ -117,11 +125,15 @@ def train_generative(data):
 
 def get_image():
     G.eval()
-    f_data = Variable(torch.rand(1, 1, 28, 28))
-    img = G(f_data)
+    z = torch.rand(1, 1, 28, 28)
+    if CUDA:
+        z = z.cuda()
+    f_data = Variable(z)
+    img = G(f_data).cpu()
     img = img.data.numpy()
     img = img.reshape(28,28)
     return img
+
 
 def plot_loss(d_loss, g_loss, step):
     fig, ax = plt.subplots()
@@ -133,27 +145,40 @@ def plot_loss(d_loss, g_loss, step):
     fig.savefig('plots/loss_{0}'.format(step))
     plt.close()
 
+
 def plot_image(img, step):
     fig, ax = plt.subplots()
     ax.imshow(img, cmap='gray')
     fig.savefig('plots/generative_img_{0}'.format(step))
     plt.close()
 
+
 def main(steps):
     d_loss = []
     g_loss = []
     for step in range(steps):
-        for batch_num, (data, y) in enumerate(train_loader):
+        dl = 0.0
+        gl = 0.0
+        for batch_num, (data, _) in enumerate(train_loader):
+            if CUDA:
+                data = data.cuda()
             dl = train_discriminative(data)
-            d_loss.append(dl)
+            # d_loss.append(dl)
             if (batch_num + 1) % k == 0:
                 gl = train_generative(data)
-                g_loss.append(gl)
-        print "Epoch {0}".format(step)
+                # g_loss.append(gl)
+        d_loss.append(dl)
+        g_loss.append(gl)
+        print "Epoch {0}, d_loss {1}, g_loss {2}".format(step, d_loss[-1], g_loss[-1])
         img = get_image()
         plot_image(img, step)
         plot_loss(np.array(d_loss)[::k], np.array(g_loss), step)
 
 
+torch.manual_seed(1)
+if CUDA:
+    torch.cuda.manual_seed(1)
+
+
 if __name__ == "__main__":
-    main(4)
+    main(30)
